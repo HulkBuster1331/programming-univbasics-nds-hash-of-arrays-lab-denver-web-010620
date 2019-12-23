78c03bf1b984d324ef8cb52bd650d1025a1d39a9
Dillon Franklin <drfranklin1331@gmail.com>
2019-12-23 20:22:17 +0000

    Automatically backed up by Learn

--- lib/hoa.rb
+++ lib/hoa.rb
@@ -6,10 +6,17 @@ BASE_HOA = {
 
 
 def add_character(show, name)
+<<<<<<< HEAD
  # binding.pry 
   BASE_HOA[show].push(name)
   
     BASE_HOA[show]
+  
+=======
+  BASE_HOA[:show][:name]
+    
+    BASE_HOA[:show]
+>>>>>>> 89614a41d13c264c33c5e4b09f17e9a89416353a
   
   
   # Write your implementation here
